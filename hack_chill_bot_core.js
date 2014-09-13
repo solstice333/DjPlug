5b4d09bb1f2a9ae26f651955c08fcd75555d24db
knavero <knavero@gmail.com>
2014-09-12 17:20:01 -0700

    added chat_every_hour()

--- hack_chill_bot_core.js
+++ hack_chill_bot_core.js
@@ -1,6 +1,15 @@
 
-responses = ["I'm sorry ChillBot. Just don't hurt me", "hello", "I'm here", 
- "Sup", "How's it going?", "Hmmmm", "Cool track", "hi"];
+responses = ["I'm liking this song", "Sup ChillBot", "This is pretty chill", 
+ "How's it going ChillBot?", "Hmmmm", "Cool track", "Sweet track", "Hey ChillBot",
+ "Chill vibe", ":)", "Nice song", "Good song", "Nice track", "Good track",
+ "Cool song", "Chill song", "Yeeeah", ":D", ":>", "So relaxing", "Very nice music",
+ "Nice music", "Good music", "Cool music", "Chill music", "Very good music", 
+ "Very chill music", "Very cool music", "Really nice song", "Really good song",
+ "Really nice track", "Really good track", "Really chill song", 
+ "Really chill track", "Sweet track", "Sweet song", "Really sweet track", 
+ "Really sweet song", "Pretty sweet track", "Pretty sweet song", 
+ "Pretty cool track", "Pretty cool song", "Pretty good track", 
+ "Pretty good song"];
 
 function chat_arrival(data) {
    if (data.un == "ChillBot") {
@@ -11,9 +20,17 @@ function chat_arrival(data) {
    }
 }
 
+function chat_every_hour() {
+   setInterval(function() { 
+      var index = Math.floor(Math.random() * responses.length);
+      API.sendChat(responses[index]);
+   }, 60 * 60 * 1000);
+}
+
 function join_waitlist() {
    API.djJoin();
 }
 
 API.on(API.CHAT, chat_arrival);
 API.on(API.ADVANCE, join_waitlist);
+chat_every_hour();
